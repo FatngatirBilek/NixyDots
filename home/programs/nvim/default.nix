f0433848cd36cd5f5d8c923b498529f8d601fdb6
FatngatirBilek <fathirbimashabri@gmail.com>
2025-01-29 14:16:03 +0700

    add nvchad config??

--- home/programs/nvim/default.nix
+++ home/programs/nvim/default.nix
@@ -3,10 +3,15 @@
   inputs,
   ...
 }: {
+  programs.neovim = {
+    enable = true;
+  };
   home.packages = with pkgs; [
-    inputs.nvchad4nix.packages."${system}".nvchad
     cargo
     deno
     opam
+    gnumake
   ];
+  xdg.configFile."nvim".source = ./config;
+  xdg.configFile."nvim".recursive = true;
 }
