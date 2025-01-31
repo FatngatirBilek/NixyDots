0020b79a75ad8fc82c82875ddc34ab4d49573462
FatngatirBilek <fathirbimashabri@gmail.com>
2025-01-31 13:26:56 +0700

    new animation

--- home/system/hyprland/animations.nix
+++ home/system/hyprland/animations.nix
@@ -3,15 +3,16 @@
     animations = {
       enabled = true;
       bezier = [
-        "cihuyy, 0.05, 0.9, 0.1, 1.05"
+        "shot, 0.02, 1.0, 0.2, 1.0"
+        "swipe, 0.6, 0.0, 0.2, 1.05"
+        "linear, 0.0, 0.0, 1.0, 1.0"
+        "progressive, 1.0, 0.0, 0.6, 1.0"
       ];
 
       animation = [
-        "windows, 1, 5, cihuyy"
-        "windowsOut, 1, 5, default, popin 80%"
-        "border, 1, 10, default"
-        "fade, 1, 5, default"
-        "workspaces, 1, 6, default"
+        "windows, 1, 6, shot, slide"
+        "workspaces, 1, 6, swipe, slide"
+        "fade, 1, 4, progressive"
       ];
     };
   };
