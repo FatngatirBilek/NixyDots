bb0cfdae83d408bc00aa6058e6f85ad31bcbe319
FatngatirBilek <fathirbimashabri@gmail.com>
2025-01-31 13:21:21 +0700

    add warp cloudflare

--- flake.nix
+++ flake.nix
@@ -42,10 +42,6 @@
     hyprpanel.url = "github:Jas-SinghFSU/HyprPanel";
     stylix.url = "github:danth/stylix";
     apple-fonts.url = "github:Lyndeno/apple-fonts.nix";
-    pia = {
-      url = "github:Fuwn/pia.nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     nur.url = "github:nix-community/NUR";
     zen-browser.url = "github:0xc000022070/zen-browser-flake"; # updated flake
   };
@@ -64,7 +60,6 @@
             inputs.nixos-hardware.nixosModules.omen-16-n0005ne # CHANGEME: check https://github.com/NixOS/nixos-hardware
             inputs.home-manager.nixosModules.home-manager
             inputs.stylix.nixosModules.stylix
-            inputs.pia.nixosModules."x86_64-linux".default
             ./hosts/laptop/configuration.nix # CHANGEME: change the path to match your host folder
           ];
         };
