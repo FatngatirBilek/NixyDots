83d768b788dca83a047b64f504b7a0165a62c682
FatngatirBilek <fathirbimashabri@gmail.com>
2025-03-07 15:43:30 +0700

    flakes update

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
   '';
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     home-manager = {
       url = "github:nix-community/home-manager";
