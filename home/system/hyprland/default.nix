f380d62f81f4ea162ec30455bf5216373cfe26e4
FatngatirBilek <fathirbimashabri@gmail.com>
2025-04-22 07:06:13 +0700

    default gpu switch

--- home/system/hyprland/default.nix
+++ home/system/hyprland/default.nix
@@ -100,7 +100,7 @@ in {
         "XDG_SESSION_TYPE,wayland"
         "SDL_VIDEODRIVER,wayland"
         "CLUTTER_BACKEND,wayland"
-        "AQ_DRM_DEVICES,/dev/dri/card1:/dev/dri/card0" # CHANGEME: Related to the GPU
+        "AQ_DRM_DEVICES,/dev/dri/card0:/dev/dri/card1" # CHANGEME: Related to the GPU
       ];
 
       cursor = {
