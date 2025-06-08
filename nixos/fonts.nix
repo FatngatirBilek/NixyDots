75d6f686b0292189e153e6bf2125be532c9e7d24
FatngatirBilek <fathirbimashabri@gmail.com>
2025-06-08 23:32:17 +0700

    add fonts

--- nixos/fonts.nix
+++ nixos/fonts.nix
@@ -1,5 +1,8 @@
-{ pkgs, inputs, ... }: {
-
+{
+  pkgs,
+  inputs,
+  ...
+}: {
   fonts = {
     packages = with pkgs; [
       roboto
@@ -21,16 +24,17 @@
       inputs.apple-fonts.packages.${pkgs.system}.sf-pro-nerd
       corefonts
       vistafonts
+      material-symbols
     ];
 
     enableDefaultPackages = false;
 
     fontconfig = {
       defaultFonts = {
-        monospace = [ "FiraCode Nerd Font Mono" "Noto Color Emoji" ];
-        sansSerif = [ "SFProDisplay Nerd Font" "Noto Color Emoji" ];
-        serif = [ "SFProDisplay Nerd Font" "Noto Color Emoji" ];
-        emoji = [ "Noto Color Emoji" ];
+        monospace = ["FiraCode Nerd Font Mono" "Noto Color Emoji"];
+        sansSerif = ["SFProDisplay Nerd Font" "Noto Color Emoji"];
+        serif = ["SFProDisplay Nerd Font" "Noto Color Emoji"];
+        emoji = ["Noto Color Emoji"];
       };
     };
   };
