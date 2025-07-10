d1ae85c19209fa6eea5a53ef672286ecb1626af6
FatngatirBilek <fathirbimashabri@gmail.com>
2025-07-10 19:42:59 +0700

    wezterm font size update

--- home/programs/wezterm/default.nix
+++ home/programs/wezterm/default.nix
@@ -7,7 +7,7 @@
 
       return {
         font = wezterm.font("JetBrainsMono Nerd Font"),
-        font_size = 13.0,
+        font_size = 14.0,
         color_scheme = "Catppuccin Mocha",
         window_background_opacity = 0.9,
 
