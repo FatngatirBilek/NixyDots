61429928d4e2733f01a4fbc3f5c8c24e6833b4e4
FatngatirBilek <fathirbimashabri@gmail.com>
2026-02-23 14:05:52 +0700

    restore

--- nixos/intel.nix
+++ nixos/intel.nix
@@ -7,6 +7,7 @@
     intel-vaapi-driver = pkgs.intel-vaapi-driver.override {enableHybridCodec = false;};
   };
   hardware.graphics = {
+    enable = true;
     # Use hardware.opengl for NixOS versions < 24.11
     extraPackages = with pkgs; [
       intel-media-driver
